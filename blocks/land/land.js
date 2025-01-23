84da1597c77ff6c2217c360ef19efe11ec138a46
Priya Srivastava <priyasr@adobe.com>
2025-01-23 10:43:52 +0530

    Initial commit

--- blocks/land/land.js
+++ blocks/land/land.js
@@ -1,8 +1,17 @@
-import { fetchPlaceholders } from "../../scripts/aem";
-// fetch placeholders sheet  from the 'en' folder
-const placeholders = await fetchPlaceholders('en');
-// retrieve the value for key 'foo'
-const { English} = placeholders;
-const landingBlock = document.getElementByClassName("land")[0];
-const para = landingBlock.getElementByTagName('p')[0];
-para.textContent=English;
+
+
+// const landingBlock = document.getElementByClassName("land")[0];
+// const para = landingBlock.getElementByTagName('p')[0];
+// para.textContent=English;
+
+import { fetchPlaceholders, getMetadata } from '../../scripts/aem.js'; 
+
+export default async function decorate(block) { 
+    
+    // fetch placeholders sheet  from the 'en' folder
+    const placeholders = await fetchPlaceholders('en'); 
+    // retrieve the value for key 'foo'
+    const { English} = placeholders;
+    console.log(English);
+
+} 
