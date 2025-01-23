b0015017ae5034e1aecab92ed55b3e396fe848a4
Priya Srivastava <priyasr@adobe.com>
2025-01-23 11:00:12 +0530

    Initial commit

--- blocks/land/land.js
+++ blocks/land/land.js
@@ -3,16 +3,18 @@
 // const landingBlock = document.getElementByClassName("land")[0];
 // const para = landingBlock.getElementByTagName('p')[0];
 // para.textContent=English;
+import { fetchPlaceholders, getMetadata } from '../../scripts/aem.js';
 
-import { fetchPlaceholders, getMetadata } from '../../scripts/aem.js'; 
+export default async function decorate(block) {
+  // Fetch placeholders sheet from the 'en' folder
+  const placeholders = await fetchPlaceholders('en');
+  console.log('Placeholders:', placeholders);
 
-export default async function decorate(block) { 
-    
-    // fetch placeholders sheet  from the 'en' folder
-    const placeholders = await fetchPlaceholders('en'); 
-    // retrieve the value for key 'foo'
-    console.log(placeholders);
-    const { english} = placeholders;
-    console.log(english);
-
-} 
+  // Safely destructure and check for undefined
+  if (placeholders) {
+    const { english } = placeholders;
+    console.log('English Placeholder:', english);
+  } else {
+    console.error('Placeholders object is null or undefined');
+  }
+}
