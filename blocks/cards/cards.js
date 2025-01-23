b94e3d7ee5e9e1f36c9e8acd6e73ae16f14779f5
Priya Srivastava <priyasr@adobe.com>
2025-01-23 11:10:11 +0530

    Initial commit

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,26 +1,27 @@
-// import { createOptimizedPicture } from '../../scripts/aem.js';
-// import { fetchPlaceholders, getMetadata } from '../../scripts/aem.js'; 
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { fetchPlaceholders, getMetadata } from '../../scripts/aem.js'; 
 
-// export default async function decorate(block) {
-//   /* change to ul, li */
-//   const ul = document.createElement('ul');
-//   [...block.children].forEach((row) => {
-//     const li = document.createElement('li');
-//     while (row.firstElementChild) li.append(row.firstElementChild);
-//     [...li.children].forEach((div) => {
-//       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-//       else div.className = 'cards-card-body';
-//     });
-//     ul.append(li);
-//   });
-//   ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-//   block.textContent = '';
-//   block.append(ul);
+export default async function decorate(block) {
+  /* change to ul, li */
+  const ul = document.createElement('ul');
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    while (row.firstElementChild) li.append(row.firstElementChild);
+    [...li.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      else div.className = 'cards-card-body';
+    });
+    ul.append(li);
+  });
+  ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  block.textContent = '';
+  block.append(ul);
 
  
 
-// const placeholders = await fetchPlaceholders(''); 
-// const {Click} = placeholders;
-// console.log(Click);
+const placeholders = await fetchPlaceholders(''); 
+console.log(placeholders);
+const {click} = placeholders;
+console.log(click);
 
-// }
+}
