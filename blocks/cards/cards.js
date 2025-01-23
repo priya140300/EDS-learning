cd8dd0d459316cd340dadaa228beada6709a10da
Priya Srivastava <priyasr@adobe.com>
2025-01-23 10:35:14 +0530

    Initial commit

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -19,7 +19,7 @@ export default async function decorate(block) {
 
  
 
-const placeholders = await fetchPlaceholders('mysite'); 
+const placeholders = await fetchPlaceholders(''); 
 const {Click} = placeholders;
 console.log(Click);
 
